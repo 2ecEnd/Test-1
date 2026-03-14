91113ca3b92eb0b8a75d6d3d6b16c0dfdf2fca67
Lee Alexander <leealex.2006@mail.ru>
2026-03-14 12:32:41 +0700

    added repositories

---
+++ src/main/java/com/example/Test_1/repositories/SchedulePeriodRepository.java
@@ -0,0 +1,9 @@
+package com.example.Test_1.repositories;
+
+import com.example.Test_1.models.entities.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface SchedulePeriodRepository extends JpaRepository<Schedule, UUID> {
+}
